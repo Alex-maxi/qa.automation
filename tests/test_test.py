c91e630acc13cd064f54307e05c6cb1a46ee091f
Alexander Sokolov <alex.sokolov.qa@gmail.com>
2023-03-14 18:12:36 +0200

    Adding folders to .gitignore

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,5 +1,21 @@
+class User:
+
+    def __init__(self, age) -> None:
+        self.age = age
+
+        def remove(self):
+            # database interaction
+            self.page = None
+
 def test_test():
-    assert 1 == 1
+    assert true
     
 def test_test():
-    assert 2 == 2    
+    assert 1 == 1   
+
+def test_user_age_is_42():
+    user = User(42)
+
+    assert user.age == 43
+
+    user.remove()
