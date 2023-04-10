fb752e4b9ed406843b63e9b0b85b8bb93a136322
Oleksandr Sokolov <oleksandr.p.sokolov@globallogic.com>
2023-04-10 17:56:37 +0300

    Adding random-text generator method.

--- src/libraries/helpers.py
+++ src/libraries/helpers.py
@@ -1,3 +1,5 @@
+import random
+import string
 from src.config.config import config
 import csv
 
@@ -10,3 +12,8 @@ def read_data_from_csv(filename):
         for rows in reader:
             datalist.append(rows)
             yield rows
+
+
+def text_generator(len: int):
+    letters = string.ascii_letters
+    return (''.join(random.choice(letters) for i in range(len)))
