04b3c3964af9707a039e0287ea032b14ff2c640c
Oleksandr Sokolov <oleksandr.p.sokolov@globallogic.com>
2023-04-10 17:58:43 +0300

    Deleting comments for tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -12,15 +12,11 @@ sum_test_data = read_data_from_csv(input_data)
 apps = Apps()
 api = GitHubApiClient()
 
-# Test with using DDT
-
 
 @pytest.mark.math
 @pytest.mark.parametrize("data, result", sum_test_data)
-def test_api_git_2(data, result):
+def test_get_sum_of_digits(data, result):
     assert apps.get_sum_of_digits(data) == int(result)
-
-# Testing User class
 
 
 @pytest.mark.set_get
@@ -29,16 +25,12 @@ def test_user_age_and_name_dict():
         'tyson@gmail.com').set_gender('Male').build_user_obj()
     assert user['age'] == 36 and user['name'] == "Tyson" and user['email'] == 'tyson@gmail.com'
 
-# Testing User class
-
 
 @pytest.mark.set_get
 def test_user_age_and_name_class():
     user = User(36, 'Tyson', 'Fury', 'tyson@gmail.com', 'Male')
     assert user.get_age() == 36 and user.get_name(
     ) == "Tyson" and user.get_gender() == 'Male'
-
-# Testing User class
 
 
 @pytest.mark.set_get
@@ -47,8 +39,6 @@ def test_user_class_remove():
     user.remove()
     assert user.get_age() == None and user.get_name(
     ) == None and user.get_gender() == None
-
-# Testing arr reverce
 
 
 @pytest.mark.math
