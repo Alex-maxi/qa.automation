56e335bdd7a013bc66b37f2a2adfc5aa75c7a453
Alexander Sokolov <alex.sokolov.qa@gmail.com>
2023-04-18 20:04:28 +0300

    Adding LoginPage tests.

--- src/application/ui/page_objects/github_base_page.py
+++ src/application/ui/page_objects/github_base_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait as Wait
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 
 class BasePage:
@@ -20,7 +21,12 @@ class BasePage:
 
     def wait_until_element_is_visible(self, locator: tuple, timeout: int = 10):
         return Wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
-        
+    
+    def get_title(self, timeout: int = 10) -> str:
+        Wait(self.driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, "title")))
+        text_title = self.driver.title
+        return text_title
+
     def click(self, locator: tuple, timeout: int = 10):
         self.wait_until_element_is_visible(locator, timeout)
         self.find(locator).click()
@@ -34,7 +40,12 @@ class BasePage:
     def get_text(self, locator: tuple, timeout: int = 10) -> str:
         self.wait_until_element_is_visible(locator, timeout)
         return self.find(locator).text
-
     
+    def get_url(self):
+        get_url = self.driver.current_url
+        return get_url
+    
+    def wait_until_url_changes(self, url: str, timeout: int = 10):
+        return Wait(self.driver, timeout).until(EC.url_changes(url))
 
     
